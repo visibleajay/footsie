724678a1ebdca38e431ed32a220ba6e925804292
Ajay <ratixind@gmail.com>
2022-06-14 16:22:41 +0530

    remove unneeded code.

--- src/app/appReducer.ts
+++ src/app/appReducer.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import footballManagerReducer from './store/footballManagerSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     footballManager: footballManagerReducer
   },
 });
